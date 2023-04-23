e292a148dbab820f50f7321e4701de6a2abe22d5
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2023-04-23 13:42:16 +0900

    feat: BaseEntity 도입
    생성 시간, 업데이트 시간 공통 관리

--- src/main/java/moment/hong/HongApplication.java
+++ src/main/java/moment/hong/HongApplication.java
@@ -2,12 +2,12 @@ package moment.hong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class HongApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HongApplication.class, args);
 	}
-
 }
---
+++ src/main/java/moment/hong/core/common/BaseEntity.java
@@ -0,0 +1,24 @@
+package moment.hong.core.common;
+
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createTime;
+
+    @LastModifiedDate
+    @Column(updatable = false)
+    private LocalDateTime updateTime;
+}

d55d0c93c79e0ebb62828993570641b785ca8bb1
HongJungWan <hjw43ok@hs.ac.kr>
2023-05-01 15:49:58 +0900

    feat(HongApplication): @EnableBatchProcessing 도입

--- src/main/java/moment/hong/HongApplication.java
+++ src/main/java/moment/hong/HongApplication.java
@@ -1,13 +1,15 @@
 package moment.hong;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableBatchProcessing
 @EnableJpaAuditing
 @SpringBootApplication
 public class HongApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(HongApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HongApplication.class, args);
+    }
 }

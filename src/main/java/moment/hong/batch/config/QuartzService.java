617acbbad7b8a33cbb4420cb5d414b98cbe557fa
HongJungWan <hjw43ok@hs.ac.kr>
2023-05-03 09:02:35 +0900

    style: 불필요한 주석 제거 및 코드 포맷팅

--- src/main/java/moment/hong/batch/config/QuartzService.java
+++ src/main/java/moment/hong/batch/config/QuartzService.java
@@ -21,29 +21,20 @@ public class QuartzService {
     @PostConstruct
     public void init() {
         try {
-            //스케줄러 초기화 -> DB도 CLAER
             scheduler.clear();
-            //Job 리스너 등록
             scheduler.getListenerManager().addJobListener(new QuartzJobListener());
-            //Trigger 리스너 등록
             scheduler.getListenerManager().addTriggerListener(new QuartzTriggerListener());
 
-            //Job에 필요한 Parameter 생성
             Map paramsMap = new HashMap<>();
-            //Job의 실행횟수 및 실행시간
             paramsMap.put("executeCount", 1);
             paramsMap.put("date", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
 
-            //Job 생성 및 Scheduler에 등록
             addJob(QuartzJob.class, "QuartzJob", "Quartz Job 입니다", paramsMap, "0/5 * * * * ?");
-
         } catch (Exception e) {
             log.error("addJob error  : {}", e);
-
         }
     }
 
-    //Job 추가
     public <T extends Job> void addJob(Class<? extends Job> job, String name, String dsec, Map paramsMap, String cron) throws SchedulerException {
         JobDetail jobDetail = buildJobDetail(job, name, dsec, paramsMap);
         Trigger trigger = buildCronTrigger(cron);
@@ -51,7 +42,6 @@ public class QuartzService {
         scheduler.scheduleJob(jobDetail, trigger);
     }
 
-    //JobDetail 생성
     public <T extends Job> JobDetail buildJobDetail(Class<? extends Job> job, String name, String desc, Map paramsMap) {
         JobDataMap jobDataMap = new JobDataMap();
         jobDataMap.putAll(paramsMap);
@@ -64,7 +54,6 @@ public class QuartzService {
                 .build();
     }
 
-    //Trigger 생성
     private Trigger buildCronTrigger(String cronExp) {
         return TriggerBuilder.newTrigger()
                 .withSchedule(CronScheduleBuilder.cronSchedule(cronExp))

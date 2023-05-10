9ff86b4efd076a24b8f5d5da65ee5d84f733c7f7
HongJungWan <hjw43ok@hs.ac.kr>
2023-05-10 09:33:02 +0900

    refactor: JWT 로그인 -> Session 로그인으로 마이그레이션
    회원가입 시, 중복 이메일 체크 기능 추가 구현

--- src/main/java/moment/hong/component/user/api/UserController.java
+++ src/main/java/moment/hong/component/user/api/UserController.java
@@ -1,8 +1,9 @@
 package moment.hong.component.user.api;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import moment.hong.component.user.api.request.UserLoginForm;
-import moment.hong.component.user.api.request.UserSingUpForm;
+import moment.hong.component.user.api.request.UserSignUpForm;
 import moment.hong.component.user.application.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,29 +15,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final UserService userService;
 
     @GetMapping("/login")
-    public String login(Model model, @ModelAttribute UserLoginForm userLoginForm) {
-        model.addAttribute("userLoginForm", userLoginForm);
+    public String login(Model model) {
+        model.addAttribute("userLoginForm", new UserLoginForm());
         return "users/login";
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute UserLoginForm userLoginForm) {
+    public String login(@ModelAttribute("userLoginForm") UserLoginForm userLoginForm) {
         userService.login(userLoginForm);
         return "redirect:/";
     }
 
     @GetMapping("/sign-up")
-    public String singUp(@ModelAttribute("userSingUpForm") UserSingUpForm userSingUpForm) {
+    public String singUp(@ModelAttribute("userSignUpForm") UserSignUpForm userSignUpForm) {
         return "users/sign-up";
     }
 
     @PostMapping("/sign-up")
-    public String signUp(@ModelAttribute("userSingUpForm") UserSingUpForm userSingUpForm) {
-        userService.join(userSingUpForm);
+    public String signUp(@ModelAttribute("userSignUpForm") UserSignUpForm userSignUpForm) {
+        userService.join(userSignUpForm);
         return "redirect:/";
     }
 }
